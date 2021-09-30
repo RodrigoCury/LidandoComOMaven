e24604bc42834303bc13622da3bbf2e0409c69cc
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-30 00:18:53 -0300

    Finalizado Curso de Maven

--- controle-loja/src/test/java/br/dev/rodrigocury/loja/ProdutoTest.java
+++ controle-loja/src/test/java/br/dev/rodrigocury/loja/ProdutoTest.java
@@ -1,14 +1,26 @@
 package br.dev.rodrigocury.loja;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		assertEquals(123L, 123L);
+		String taErrado = "TA ERRADO";
+		
+		Produto p = new Produto("Rodrigo");
+		
+		Assert.assertEquals(taErrado , "Rodrigo", p.getNome());
+		
+		p.setNome("batatinha");
+		
+		Assert.assertEquals(taErrado , "batatinha", p.getNome());
+		
+		Produto p1 = new Produto("batatinha");
+		
+		Assert.assertEquals(taErrado , p, p1);
+		
 	}
 
 }
